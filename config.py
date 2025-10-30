470556429b5a6faa6294d4420a231a5d5ca4950f
sha-md <135060511+sha-md@users.noreply.github.com>
2025-10-30 21:42:38 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+from member import Member
+
+TITLE = "London Fire Brigade Analysis Dashboard"
+PROMOTION = "Data Science Project 2023"
+
+TEAM_MEMBERS = [
+    Member(
+        name="Your Name",
+        linkedin_url="https://www.linkedin.com/in/yourprofile/",
+        github_url="https://github.com/yourgithub",
+    )
+]
