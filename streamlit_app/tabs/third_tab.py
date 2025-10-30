da071496ed08b9cb999fc4d5dd97e9d46d2d7fa5
sha-md <135060511+sha-md@users.noreply.github.com>
2025-10-30 21:55:14 +0100

    Delete streamlit_app/tabs directory

--- streamlit_app/tabs/third_tab.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-sidebar_name = "Modelling"
-
-def run():
-    st.header("🤖 Predictive Modelling")
-    st.write("In this section, you can include ML models for fire incident prediction, response time, etc.")
